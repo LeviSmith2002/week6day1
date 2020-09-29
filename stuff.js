a3506f675bb10c2da2cc020afba13afb31afec90
Levi Smith <student@Mountainlands-Mac-mini.local>
2020-09-28 20:56:45 -0600

    Added Week6Day1

---
+++ stuff.js
@@ -0,0 +1,78 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+storeItems.forEach(function(item){
+    if (item.inStock === true) {
+    $('.bod').append(`<div class='thingy' class="darkMode"><span>$${item.price}</span> <span>${item.name}</span> <span>${item.details}</span></div>`);
+    } else {}
+});
+
+$('.button').click(function(e){
+    $('body').toggleClass(`darkMode`);
+});
